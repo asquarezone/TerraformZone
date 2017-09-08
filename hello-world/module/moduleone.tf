efca1f27163ea32bbfc563e55d5971d80d201fcd
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-09-08 05:57:34 +0530

    Added terraform helloworld

---
+++ hello-world/module/moduleone.tf
@@ -0,0 +1,50 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "buildserver"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+resource "aws_instance" "nginx" {
+  ami           = "ami-c58c1dd3"
+  instance_type = "t2.micro"
+  key_name        = "${var.key_name}"
+
+  connection {
+    user        = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+    value = "${aws_instance.nginx.public_dns}"
+}
---
+++ hello-world/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_access_key = "<insert access key here>"
+
+aws_secret_key = "<insert secret key here>"
+
+private_key_path = "<path to private key, use \\ for any \ in path>"
