6c5dfb517dcf35bddc00d4cfaaa3e97241791454
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-09-14 05:27:17 +0530

    Added necessary example

---
+++ second/moduletwo.tf
@@ -0,0 +1,91 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "test"
+}
+variable "vpc_id" {}
+
+
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_vpc" "selected" {
+  id = "${var.vpc_id}"
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+# Nginx security group 
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx_sg"
+  vpc_id      = "${data.aws_vpc.selected.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami           = "ami-c58c1dd3"
+  instance_type = "t2.micro"
+  key_name        = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
+
+  connection {
+    user        = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+    value = "${aws_instance.nginx.public_dns}"
+}
