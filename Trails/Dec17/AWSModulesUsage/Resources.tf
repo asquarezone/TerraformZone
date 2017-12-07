6c8f7c7ad32d3d206c3c856c92b07a0bc0f7daac
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-12-07 16:56:43 +0530

    Added module usage

---
+++ Trails/Dec17/AWSModulesUsage/Resources.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = "us-west-2"
+    access_key = ""
+    secret_key = ""
+}
+
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    name = "my-vpc"
+    cidr = "10.0.0.0/16"
+    azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+    enable_nat_gateway = true
+    enable_vpn_gateway = true
+
+    tags = {
+        Terraform = "true"
+        Environment = "dev"
+    }
+
+
+}
