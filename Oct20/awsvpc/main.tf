cb342e273ad5b14e47f1e4fd4da175e556b8e5e5
qtkhaja <qtkhaja@gmail.com>
2020-10-08 08:38:21 +0530

    Updated Web Subnet

--- Oct20/awsvpc/main.tf
+++ Oct20/awsvpc/main.tf
@@ -1,7 +1,20 @@
+# VPC Section
 resource "aws_vpc" "myvpc" {
     cidr_block              = "192.168.0.0/16"
     enable_dns_support      = true
     tags = {
         Name = "fromtf"
     }
+}
+
+# Web Subnet
+resource "aws_subnet" "web" {
+    vpc_id                  = aws_vpc.myvpc.id
+    cidr_block              = "192.168.0.0/24"
+    availability_zone       = "us-west-2a"
+    
+    tags = {
+        Name = "web"
+    }
+
 }
