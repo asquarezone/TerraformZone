d0d43665ed8deeefb77a63264ab882e272d5c1dd
qtkhaja <qtkhaja@gmail.com>
2020-10-08 08:44:15 +0530

    Updated other subnets

--- Oct20/awsvpc/main.tf
+++ Oct20/awsvpc/main.tf
@@ -12,9 +12,42 @@ resource "aws_subnet" "web" {
     vpc_id                  = aws_vpc.myvpc.id
     cidr_block              = "192.168.0.0/24"
     availability_zone       = "us-west-2a"
-    
+
     tags = {
         Name = "web"
+    }
+
+}
+
+resource "aws_subnet" "app" {
+    vpc_id                  = aws_vpc.myvpc.id
+    cidr_block              = "192.168.1.0/24"
+    availability_zone       = "us-west-2b"
+
+    tags = {
+        Name = "app"
+    }
+
+}
+
+resource "aws_subnet" "db" {
+    vpc_id                  = aws_vpc.myvpc.id
+    cidr_block              = "192.168.2.0/24"
+    availability_zone       = "us-west-2c"
+
+    tags = {
+        Name = "db"
+    }
+
+}
+
+resource "aws_subnet" "mgmt" {
+    vpc_id                  = aws_vpc.myvpc.id
+    cidr_block              = "192.168.3.0/24"
+    availability_zone       = "us-west-2a"
+
+    tags = {
+        Name = "mgmt"
     }
 
 }
