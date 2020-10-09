feec833e563bfdd0f2f3ce035345d83182db1545
qtkhaja <qtkhaja@gmail.com>
2020-10-09 08:01:55 +0530

    Added support for variables region and vpc cidr

---
+++ Oct20/awsvpc/inputs.tf
@@ -0,0 +1,12 @@
+# variable for region
+variable region {
+  type        = string
+  default     = "us-west-2"
+  description = "target region for creating resources"
+}
+
+variable vpc_cidr {
+  type        = string
+  default     = "192.168.0.0/16"
+  description = "cidr range of vpc"
+}
--- Oct20/awsvpc/main.tf
+++ Oct20/awsvpc/main.tf
@@ -1,6 +1,6 @@
 # VPC Section
 resource "aws_vpc" "myvpc" {
-    cidr_block              = "192.168.0.0/16"
+    cidr_block              = var.vpc_cidr
     enable_dns_support      = true
     tags = {
         Name = "fromtf"
