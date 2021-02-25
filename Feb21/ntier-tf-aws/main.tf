4f4e83eda5a00712abd7f61ee83deb572a60810c
qtdevops <qtdevops@gmail.com>
2021-02-25 08:42:45 +0530

    made small changes

--- Feb21/ntier-tf-aws/main.tf
+++ Feb21/ntier-tf-aws/main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 # we need to create a vpc resource
 resource "aws_vpc" "ntiervpc" {
     cidr_block = "192.168.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
 
     tags = {
       "Name" = "from tf"
