04aca88ffbdd7c1b5db987a17506e88dd1fcfd1b
qtdevops <qtdevops@gmail.com>
2021-03-02 08:28:56 +0530

    Added changes to accomodate dynamic subnet selection

--- Feb21/ntier-tf-aws/main.tf
+++ Feb21/ntier-tf-aws/main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "subnets" {
   count = 6
   vpc_id = aws_vpc.ntiervpc.id
   cidr_block = cidrsubnet(var.vpccidr,8,count.index)
-  availability_zone = var.subnetazs[count.index]
+  availability_zone = "${var.region}${count.index%2 == 0?"a":"b"}"
 
   tags = {
       "Name" = var.subnets[count.index]
--- Feb21/ntier-tf-aws/variables.tf
+++ Feb21/ntier-tf-aws/variables.tf
@@ -15,10 +15,3 @@ variable "subnets" {
     default = ["web1", "web2", "app1", "app2", "db1", "db2"]
     description = "Name of the subnets"
 }
-
-
-variable "subnetazs" {
-    type = list(string)
-    default = ["us-west-2a","us-west-2b","us-west-2a","us-west-2b","us-west-2a","us-west-2b"]
-  
-}
