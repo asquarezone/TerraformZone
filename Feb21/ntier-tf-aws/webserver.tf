ef4211fabacfaf2159f69bc880303658dfc04103
qtdevops <qtdevops@gmail.com>
2021-03-09 08:38:11 +0530

    Added app server instance

--- Feb21/ntier-tf-aws/webserver.tf
+++ Feb21/ntier-tf-aws/webserver.tf
@@ -1,3 +1,17 @@
+resource "aws_instance" "appserver1" {
+    ami = data.aws_ami.ubuntu.id
+    associate_public_ip_address = false
+    instance_type = var.webserverinstancetype
+    key_name = "terraform"
+    security_groups = [ aws_security_group.appsg.id ]
+    subnet_id = aws_subnet.subnets[2].id
+    tags = {
+      "Name" = "appserver 1"
+    }
+    depends_on = [ aws_db_instance.ntierdb ]
+  
+}
+
 resource "aws_instance" "webserver1" {
     ami = data.aws_ami.ubuntu.id
     associate_public_ip_address = true
