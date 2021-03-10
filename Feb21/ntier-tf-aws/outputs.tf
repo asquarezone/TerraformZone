aa4369b582b6580cfc6913829f50be2f4985a504
qtdevops <qtdevops@gmail.com>
2021-03-10 08:55:00 +0530

    Added outputs

---
+++ Feb21/ntier-tf-aws/outputs.tf
@@ -0,0 +1,18 @@
+output "vpcid" {
+    value = aws_vpc.ntiervpc.id
+  
+}
+output "webserverid" {
+    value = aws_instance.webserver1.id
+  
+}
+
+output "webserverip" {
+    value = aws_instance.webserver1.public_ip
+}
+
+output "url" {
+    # http://webserverip/info.php
+    value = format("http://%s/info.php",aws_instance.webserver1.public_ip)
+  
+}
