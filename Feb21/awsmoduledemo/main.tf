4de61bf23c40ebf7fb12a020ec4f311fe24b8c15
qtdevops <qtdevops@gmail.com>
2021-03-11 08:55:37 +0530

    AWS registry module

---
+++ Feb21/awsmoduledemo/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+  # insert the 49 required variables here
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
