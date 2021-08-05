5a550ae6a4bbfdaeca084dc0396ba9d5197d69f5
qtdevops <qtdevops@gmail.com>
2021-08-05 08:51:32 +0530

    Added ntier web subnet

--- Aug21/ntieraws/main.tf
+++ Aug21/ntieraws/main.tf
@@ -19,3 +19,15 @@ resource "aws_vpc" "ntiervpc" {
     }
   
 }
+
+# depends on AWS VPC
+resource "aws_subnet" "websubnet" {
+
+    cidr_block = "10.10.0.0/24"
+    availability_zone = "ap-south-1a"
+    tags = {
+      "Name" = "ntier-web"
+    }
+    vpc_id = aws_vpc.ntiervpc.id
+  
+}
