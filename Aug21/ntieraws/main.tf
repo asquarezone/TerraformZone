65e8f0049b62fcfefb6916bc4fe923b8ebac16af
qtdevops <qtdevops@gmail.com>
2021-08-07 20:33:33 +0530

    Added security group and ec2 instance

--- Aug21/ntieraws/main.tf
+++ Aug21/ntieraws/main.tf
@@ -55,3 +55,43 @@ resource "aws_route_table_association" "publicrtassociations" {
   subnet_id = aws_subnet.subnets[var.web_subnet_indexes[count.index]].id
   route_table_id = aws_route_table.publicrt.id
 }
+
+resource "aws_security_group" "websg" {
+  name = "openhttp"
+  description = "Open http and ssh"
+  vpc_id = aws_vpc.ntiervpc.id
+
+  tags = {
+    "Name" = "Openhttp"
+  }
+
+}
+
+resource "aws_security_group_rule" "websghttp" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.websg.id
+  
+}
+
+resource "aws_security_group_rule" "websgssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.websg.id
+  
+}
+
+resource "aws_instance" "webserver1" {
+  ami = "ami-0c1a7f89451184c8b" # ami for ubuntu in mumbai
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id = aws_subnet.subnets[0].id
+  
+}
