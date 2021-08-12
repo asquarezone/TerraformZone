cb511a7797a3b88f73a8860503aef91e63eef228
qtpython <qtkhajacloud@gmail.com>
2021-08-12 08:47:04 +0530

    Added a module from git

---
+++ Aug21/modulefromgit/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "awsvpc" {
+    source = "git::https://github.com/asquarezone/awsvpc.git?ref=1.0"
+    ntier_cidr = "10.1.0.0/16"
+    ntier_subnet_tags = ["ntier-web1", "ntier-app1", "ntier-db1", "ntier-web2", "ntier-app2", "ntier-db2"]
+    ntier_subnet_azs = ["ap-south-1a","ap-south-1a","ap-south-1a", "ap-south-1b","ap-south-1b","ap-south-1b"]
+    web_subnet_indexes = [ 0,3]
+    other_subnet_indexes = [ 1,2,4,5]
+  
+}
+
+output "vpcid" {
+   value = module.awsvpc.vpcid
+  
+}
