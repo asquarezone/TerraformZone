c7aaa31ea07bd641ccd410fc68a6be1992b86257
qtdevops <qtdevops@gmail.com>
2021-12-18 08:12:11 +0530

    Added a variable

---
+++ Dec21/activity2/inputs.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr" {
+    default = "192.168.0.0/16"
+    description = "This is the VPC cidr"
+    type = string
+}
--- Dec21/activity2/network.tf
+++ Dec21/activity2/network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "primary_vpc" {
-  cidr_block = "192.168.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
       Name = "primary"
   }
