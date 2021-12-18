9d3aa52e8d7b6792b8cfecef4fb74e0d753e8394
qtdevops <qtdevops@gmail.com>
2021-12-18 09:00:10 +0530

    Added count

--- Dec21/activity2/network.tf
+++ Dec21/activity2/network.tf
@@ -6,40 +6,15 @@ resource "aws_vpc" "primary_vpc" {
 }
 
 
-resource "aws_subnet" "web_1" {
-  vpc_id = aws_vpc.primary_vpc.id
-  availability_zone = var.subnet_azs[0]
-  cidr_block = var.subnet_cidrs[0]
-  tags = {
-      Name = var.subnet_names[0]
-  }
-}
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidrs)
 
-resource "aws_subnet" "web_2" {
   vpc_id = aws_vpc.primary_vpc.id
-  availability_zone = var.subnet_azs[1]
-  cidr_block = var.subnet_cidrs[1]
-  tags = {
-      Name = var.subnet_names[1]
-  }
-}
+  availability_zone = var.subnet_azs[count.index]
+  cidr_block = var.subnet_cidrs[count.index]
 
-
-resource "aws_subnet" "db_1" {
-  vpc_id = aws_vpc.primary_vpc.id
-  availability_zone = var.subnet_azs[2]
-  cidr_block = var.subnet_cidrs[2]
   tags = {
-      Name = var.subnet_names[2]
-  }
-}
-
-resource "aws_subnet" "db_2" {
-  vpc_id = aws_vpc.primary_vpc.id
-  availability_zone = var.subnet_azs[3]
-  cidr_block = var.subnet_cidrs[3]
-  tags = {
-      Name = var.subnet_names[3]
+      Name = var.subnet_names[count.index]
   }
 }
 
