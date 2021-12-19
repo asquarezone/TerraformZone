64a9bd47bd69732a8cb3de0662fc1123fe7c5f1b
qtdevops <qtdevops@gmail.com>
2021-12-19 08:49:23 +0530

    Added security group for web servers

--- Dec21/activity2/network.tf
+++ Dec21/activity2/network.tf
@@ -18,3 +18,45 @@ resource "aws_subnet" "subnets" {
   }
 }
 
+# Web Security Groups
+
+resource "aws_security_group" "websg" {
+  vpc_id = aws_vpc.primary_vpc.id
+
+  ingress {
+    description = "Open SSH For all"
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [ "0.0.0.0/0" ]
+
+  }
+
+  ingress {
+    description = "Open HTTP For all"
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "TCP"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "WebSg" 
+  }
+  
+}
