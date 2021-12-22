f164fc9c3e50501ce80f47a48eb308e8872503e9
qtdevops <qtdevops@gmail.com>
2021-12-22 08:44:25 +0530

    Added necessary connectivity settings

--- Dec21/activity2/network.tf
+++ Dec21/activity2/network.tf
@@ -85,3 +85,56 @@ resource "aws_security_group" "dbsg" {
   }
   
 }
+
+# Creating internet gateway
+resource "aws_internet_gateway" "ntier_igw" {
+  tags      = {
+    Name    = "ntier-igw"
+  }
+  vpc_id    = aws_vpc.primary_vpc.id
+  
+}
+
+# create a route table
+resource "aws_route_table" "public_rt" {
+  vpc_id      = aws_vpc.primary_vpc.id
+  tags        = {
+    Name      = "public"
+  }
+
+  route {
+    cidr_block = local.anywhere
+    gateway_id = aws_internet_gateway.ntier_igw.id
+  }
+  
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id      = aws_vpc.primary_vpc.id
+  tags        = {
+    Name      = "private"
+  }
+  
+}
+
+# Associate public route table with web subnets
+
+resource "aws_route_table_association" "web1_public_association" {
+  route_table_id  = aws_route_table.public_rt.id
+  subnet_id       = aws_subnet.subnets[0].id
+}
+
+resource "aws_route_table_association" "web2_public_association" {
+  route_table_id  = aws_route_table.public_rt.id
+  subnet_id       = aws_subnet.subnets[1].id
+}
+
+resource "aws_route_table_association" "db1_private_association" {
+  route_table_id  = aws_route_table.private_rt.id
+  subnet_id       = aws_subnet.subnets[2].id
+}
+
+resource "aws_route_table_association" "db2_private_association" {
+  route_table_id  = aws_route_table.private_rt.id
+  subnet_id       = aws_subnet.subnets[3].id
+}
