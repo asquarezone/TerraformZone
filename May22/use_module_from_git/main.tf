ff50cdd2cfae2b3c13641f1bd8a71e4a30c1e8a5
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-06-04 21:05:51 +0530

    Added module usage

---
+++ May22/use_module_from_git/main.tf
@@ -0,0 +1,19 @@
+module "ntier_azure" {
+    source              = "git::https://github.com/GitPracticeRepo/azure_ntier.git"
+    network_cidr        = ["192.168.0.0/16"]
+    subnet_names        = ["web", "app", "cache", "db"]
+    appsubnet           = "app"
+    websubnet           = "web"
+    servername          = "qt1dbfrommodule"
+    dbname              = "qt1ecommerce"
+    vmsize              = "Standard_B1s"
+    username            = "qtdevops"
+    password            = "motherindia@123"
+    increment_execute   = "0"
+
+}
+
+output "webvm" {
+    value = module.ntier_azure.webserver_url
+
+}
