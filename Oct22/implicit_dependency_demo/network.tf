a02df5c02a4b1f8207bd6a8fd9160f6d682e3570
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-10-16 08:35:45 +0530

    implicit

---
+++ Oct22/implicit_dependency_demo/network.tf
@@ -0,0 +1,41 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+        Name = "myvpc"
+    }
+  
+}
+
+resource "aws_subnet" "first_subnet" {
+    vpc_id = aws_vpc.my_vpc.id # implicit dependecy
+    cidr_block = "192.168.0.0/24"
+
+    tags = {
+      "Name" = "first subnet"
+    }
+  
+}
+
+resource "aws_subnet" "second_subnet" {
+    vpc_id = aws_vpc.my_vpc.id # implicit dependecy
+    cidr_block = "192.168.1.0/24"
+
+    tags = {
+      "Name" = "second subnet"
+    }
+
+    
+  
+}
+
+resource "aws_subnet" "third_subnet" {
+    vpc_id = aws_vpc.my_vpc.id # implicit dependecy
+    cidr_block = "192.168.2.0/24"
+
+    tags = {
+      "Name" = "third subnet"
+    }
+
+   
+  
+}
