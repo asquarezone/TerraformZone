73405aaaa15b1dc5247f9d3e22de2d562e2a1c94
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-10-22 19:46:03 +0530

    Added modules demo

---
+++ Oct22/module-usage-demo/main.tf
@@ -0,0 +1,19 @@
+module "myvnet" {
+    source = "./modules/myvnet"
+    resourcegroup_details = {
+      location = "eastus"
+      name = "ntier-rg"
+    }
+    vnet_details = {
+      address_space = [ "10.10.0.0/16" ]
+      name = "ntier"
+    }
+    subnet_details = {
+        names = [ "web", "db" ]
+    }
+    
+}
+
+output "subnetcountfrommodule" {
+    value = module.myvnet.subnetcount
+}
---
+++ Oct22/module-usage-demo/modules/myvnet/inputs.tf
@@ -0,0 +1,26 @@
+variable "resourcegroup_details" {
+    type = object({
+            name=string
+            location=string
+            })
+    default = {
+      location = "eastus"
+      name = "ntier"
+    }
+}
+
+variable "vnet_details" {
+    type = object({
+        name=string
+        address_space=list(string)
+        
+    })
+    description = "(optional) describe your variable"
+}
+
+variable "subnet_details" {
+    type = object({
+        names=list(string)
+    })
+  
+}
