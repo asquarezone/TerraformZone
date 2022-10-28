ca13ed9560c78f02e8af8fd94a93cacdd5f2f32f
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-10-28 08:14:09 +0530

    Added chagnes

---
+++ Oct22/taint_demo/inputs.tf
@@ -0,0 +1,20 @@
+variable "region" {
+    type        = string
+    description = "(optional) region where the resources will be created"
+    default     = "us-west-2"
+}
+
+variable "cidr_block" {
+    type        = string
+    default     = "192.168.0.0/16"
+}
+
+
+variable "subnet_name_tags" {
+    type        = list(string)
+  
+}
+
+variable "subnet_azs" {
+    type        = list(string)
+}
