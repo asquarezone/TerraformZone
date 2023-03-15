2171d9ca7e5df5f6d193641ddd09abfdc3d78e9a
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-03-15 08:32:43 +0530

    Added hello tf

---
+++ Mar23/hellotf/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "qt-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
