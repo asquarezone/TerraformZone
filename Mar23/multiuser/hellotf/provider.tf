84b6692c43a357dea049b69599993ef62b73f63e
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-03-29 08:48:57 +0530

    Added backend

--- Mar23/multiuser/hellotf/provider.tf
+++ Mar23/multiuser/hellotf/provider.tf
@@ -1,11 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.60.0"
     }
   }
   required_version = "> 1.0.0"
+  backend "s3" {
+    bucket         = "terraformremotebackendqt"
+    key            = "classes/hellotf"
+    dynamodb_table = "terraformlock"
+    region         = "us-west-2"
+
+  }
 }
 
 provider "aws" {
