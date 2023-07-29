5d20b10dfe611bb6a2588f74723e48b4b002a01a
qtkhajadevops <qtkhajadevops@gmail.com>
2023-07-29 08:54:21 +0530

    Added changes

--- july23/activity3/imnothere.tf
+++ july23/activity3/imnothere.tf
@@ -1,19 +1,19 @@
 provider "azurerm" {
-    features {     
-    }
-  
+  features {
+  }
+
 }
 
 resource "azurerm_resource_group" "myresg" {
-  name = "fromtf"
+  name     = "fromtf"
   location = "eastus"
 }
 
 resource "azurerm_storage_account" "first" {
-  name = "fromtffortf"
-  resource_group_name = azurerm_resource_group.myresg.name
-  location = azurerm_resource_group.myresg.location
+  name                     = "fromtffortf"
+  resource_group_name      = azurerm_resource_group.myresg.name
+  location                 = azurerm_resource_group.myresg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  
+
 }
