dc0107cb9ba9d7a8814a27f5a76c378af3a85a0a
qtkhajadevops <qtkhajadevops@gmail.com>
2023-08-03 08:38:22 +0530

    Added changes

--- july23/ntier-aws/inputs.tf
+++ july23/ntier-aws/inputs.tf
@@ -4,9 +4,20 @@ variable "vpc_network_cidr" {
   description = "This is network cidr"
 }
 
-variable "web1_subnet_cidr" {
-  type        = string
-  default     = "10.10.0.0/24"
-  description = "This is web1 subnet cidr"
+variable "subnet_cidr_ranges" {
+  type        = list(string)
+  default     = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+  description = "This is network cidr ranges"
+}
 
+variable "subnet_names" {
+  type        = list(string)
+  default     = ["web1", "web2", "app1", "app2", "db1", "db2"]
+  description = "These are subnet names"
+
+}
+
+variable "subnet_count" {
+  type    = number
+  default = 6
 }
--- july23/ntier-aws/main.tf
+++ july23/ntier-aws/main.tf
@@ -6,12 +6,15 @@ resource "aws_vpc" "ntier_vpc" {
 }
 
 
-resource "aws_subnet" "web1" {
-  vpc_id     = aws_vpc.ntier_vpc.id
-  cidr_block = var.web1_subnet_cidr
 
+resource "aws_subnet" "subnets" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.ntier_vpc.id
+  cidr_block = var.subnet_cidr_ranges[count.index]
+  tags = {
+    Name = var.subnet_names[count.index]
+  }
   depends_on = [
     aws_vpc.ntier_vpc
   ]
-
 }
