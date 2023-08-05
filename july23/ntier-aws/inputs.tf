a964d38011911412aed724953d6dd08981fde4e0
qtkhajadevops <qtkhajadevops@gmail.com>
2023-08-05 08:29:31 +0530

    Added cidrsubnet

--- july23/ntier-aws/inputs.tf
+++ july23/ntier-aws/inputs.tf
@@ -4,13 +4,6 @@ variable "vpc_network_cidr" {
   description = "This is network cidr"
 }
 
-variable "subnet_cidr_format" {
-  type    = string
-  default = "10.10.%g.0/24"
-
-}
-
-
 variable "subnet_names" {
   type        = list(string)
   default     = ["web1", "web2", "app1", "app2", "db1", "db2"]
--- july23/ntier-aws/main.tf
+++ july23/ntier-aws/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "ntier_vpc" {
 resource "aws_subnet" "subnets" {
   count      = length(var.subnet_names)
   vpc_id     = aws_vpc.ntier_vpc.id
-  cidr_block = format(var.subnet_cidr_format, count.index)
+  cidr_block = cidrsubnet(var.vpc_network_cidr, 8, count.index)
   tags = {
     Name = var.subnet_names[count.index]
   }
