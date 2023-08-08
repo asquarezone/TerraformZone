fceb15ec9ae5a27396d643c19a7b0ee550b0ee23
qtkhajadevops <qtkhajadevops@gmail.com>
2023-08-08 08:59:48 +0530

    Added rds instance

--- july23/ntier-aws/database.tf
+++ july23/ntier-aws/database.tf
@@ -26,7 +26,22 @@ resource "aws_db_subnet_group" "ntier" {
 
 }
 
-# resource "aws_db_instance" "ntierdb" {
+resource "aws_db_instance" "ntier" {
+  allocated_storage      = 20
+  db_name                = "emp"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = "root"
+  password               = "rootroot"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.ntier.name
+  identifier             = "qtntierdbfromtf"
+  vpc_security_group_ids = [aws_security_group.dbsg.id]
 
-
-# }
+  depends_on = [
+    aws_db_subnet_group.ntier,
+    aws_vpc.ntier_vpc,
+    aws_subnet.subnets
+  ]
+}
