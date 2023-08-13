ae8fd24751687477de9dc238d189856f8463a205
qtkhajadevops <qtkhajadevops@gmail.com>
2023-08-13 19:11:51 +0530

    Added changes

--- july23/ntier-aws/providers.tf
+++ july23/ntier-aws/providers.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.4.0"
+  backend "s3" {
+    bucket = "qtcommonstate"
+    key ="ntier-aws/terraform"
+    region = "ap-south-1"
+    dynamodb_table = "qtterraformstate"
+  }
 }
 
 provider "aws" {
