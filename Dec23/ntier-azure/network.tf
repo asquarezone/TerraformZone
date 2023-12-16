92c18456ed96526e5ab812555da6e50ccb588f81
qtdevops <qtdevops@gmail.com>
2023-12-16 09:07:01 +0530

    After formatting

--- Dec23/ntier-azure/network.tf
+++ Dec23/ntier-azure/network.tf
@@ -1,37 +1,37 @@
 resource "azurerm_resource_group" "rg" {
-    name = "ntier"
-    location = "eastus"
+  name     = "ntier"
+  location = "eastus"
 }
 
 
 resource "azurerm_virtual_network" "primarynet" {
-    name = "ntier-primary"
-    resource_group_name = "ntier"
-    address_space = [ "10.100.0.0/16" ]
-    location = "eastus"
-  
+  name                = "ntier-primary"
+  resource_group_name = "ntier"
+  address_space       = ["10.100.0.0/16"]
+  location            = "eastus"
+
 }
 
 resource "azurerm_subnet" "web" {
-    name = "web"
-    resource_group_name = "ntier"
-    virtual_network_name = "ntier-primary"
-    address_prefixes = [ "10.100.0.0/24" ]
-  
+  name                 = "web"
+  resource_group_name  = "ntier"
+  virtual_network_name = "ntier-primary"
+  address_prefixes     = ["10.100.0.0/24"]
+
 }
 
 resource "azurerm_subnet" "business" {
-    name = "business"
-    resource_group_name = "ntier"
-    virtual_network_name = "ntier-primary"
-    address_prefixes = [ "10.100.1.0/24" ]
-  
+  name                 = "business"
+  resource_group_name  = "ntier"
+  virtual_network_name = "ntier-primary"
+  address_prefixes     = ["10.100.1.0/24"]
+
 }
 
 resource "azurerm_subnet" "data" {
-    name = "data"
-    resource_group_name = "ntier"
-    virtual_network_name = "ntier-primary"
-    address_prefixes = [ "10.100.2.0/24" ]
-  
+  name                 = "data"
+  resource_group_name  = "ntier"
+  virtual_network_name = "ntier-primary"
+  address_prefixes     = ["10.100.2.0/24"]
+
 }
--- Dec23/ntier-azure/providers.tf
+++ Dec23/ntier-azure/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.85.0"
     }
   }
@@ -9,8 +9,8 @@ terraform {
 
 
 provider "azurerm" {
-    features {
-      
-    }
-  
+  features {
+
+  }
+
 }
