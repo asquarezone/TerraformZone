93bebfbe9779460e00b931294cf94ce3e58192a4
qtdevops <qtdevops@gmail.com>
2023-12-19 08:58:36 +0530

    Added business and data

--- Dec23/ntier-aws/network.tf
+++ Dec23/ntier-aws/network.tf
@@ -15,3 +15,19 @@ resource "aws_subnet" "web" {
     Name = "web"
   }
 }
+
+resource "aws_subnet" "business" {
+  cidr_block = "10.100.1.0/24"
+  vpc_id     = aws_vpc.primary_network.id
+  tags = {
+    Name = "business"
+  }
+}
+
+resource "aws_subnet" "data" {
+  cidr_block = "10.100.2.0/24"
+  vpc_id     = aws_vpc.primary_network.id
+  tags = {
+    Name = "data"
+  }
+}
