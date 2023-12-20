ff916abcecd080fe087cbeeaf1752fed12c90d7a
qtdevops <qtdevops@gmail.com>
2023-12-20 09:01:07 +0530

    Added length function

--- Dec23/ntier-aws/network.tf
+++ Dec23/ntier-aws/network.tf
@@ -1,15 +1,13 @@
 resource "aws_vpc" "primary_network" {
   cidr_block = var.vpc_cidr
-
   tags = {
     Name = "ntier"
   }
-
 }
 
 
 resource "aws_subnet" "subnets" {
-  count      = var.subnetcount
+  count      = length(var.subnet_cidrs)
   vpc_id     = aws_vpc.primary_network.id
   cidr_block = var.subnet_cidrs[count.index]
 }
