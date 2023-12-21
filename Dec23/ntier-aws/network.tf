dec2a88ea55f5799360b0800a7656eb059951262
qtdevops <qtdevops@gmail.com>
2023-12-21 08:34:39 +0530

    Added cidr subnet function

--- Dec23/ntier-aws/network.tf
+++ Dec23/ntier-aws/network.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "primary_network" {
 resource "aws_subnet" "subnets" {
   count      = length(var.subnet_names)
   vpc_id     = aws_vpc.primary_network.id
-  cidr_block = "10.100.${count.index}.0/24"
+  cidr_block = cidrsubnet(var.vpc_cidr,8,count.index)
   tags = {
     Name = var.subnet_names[count.index]
   }
