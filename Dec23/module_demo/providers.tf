f21c00a7cd5294b978e0ce1d54716c20a68fbf63
qtdevops <qtdevops@gmail.com>
2023-12-24 17:49:32 +0530

    Added module usage demo

---
+++ Dec23/module_demo/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 5.31.0"
+    }
+  }
+  // required_version = "> 1.6.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  default_tags {
+    tags = {
+      Env       = "Dev"
+      CreatedBy = "Teraform"
+    }
+
+  }
+}
