d694a30626c87e3d4a119cfc70138c4464b0056c
qtdevops <qtdevops@gmail.com>
2023-12-26 09:14:00 +0530

    Added changes to include azurerm backend

--- Dec23/ntier-azure/providers.tf
+++ Dec23/ntier-azure/providers.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
   required_version = "> 1.5.0"
+  backend "azurerm" {
+    resource_group_name = "state"
+    storage_account_name = "qtterraformdec23"
+    container_name = "states"
+    key = "ntier-azure.tfstate"
+    
+  }
 }
 
 provider "azurerm" {
