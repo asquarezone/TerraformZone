93e1e89287cf17a52c51c95b22ed2373f3c0559f
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-05-20 08:36:19 +0530

    Added changes

--- May24/provisioner/providers.tf
+++ May24/provisioner/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.50.0"
     }
   }
+  backend "s3" {
+    bucket = "terraformbackendqt"
+    key = "learning/provisioners"
+    region = "ap-south-1"
+    dynamodb_table = "forlocking"
+    
+  }
 }
 
 provider "aws" {
