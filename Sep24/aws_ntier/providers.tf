290a2703107fade403388d84901d2022ccdfe37e
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-09-21 09:58:07 +0530

    Added changes

--- Sep24/aws_ntier/providers.tf
+++ Sep24/aws_ntier/providers.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.68.0"
+      version = "~= 5.64.9"
     }
   }
+  required_version = ">= 1.9.0"
 }
 
 provider "aws" {
