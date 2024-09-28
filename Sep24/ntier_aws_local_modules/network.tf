d6b2cf00c9cf69a8abaac985d3736784f18861bc
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-09-28 08:49:43 +0530

    Added usage of security group module

--- Sep24/ntier_aws_local_modules/network.tf
+++ Sep24/ntier_aws_local_modules/network.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-    source = "./modules/vpc"
-    vpc_info = {
+  source = "git::https://github.com/asquarezone/LTTerraformModules.git//aws/vpc"
+  vpc_info = {
     cidr_block           = "192.168.0.0/16"
     enable_dns_hostnames = true
     tags = {
@@ -24,5 +24,31 @@ module "vpc" {
     }
 
   ]
-  
+
+}
+
+module "websg" {
+  source = "git::https://github.com/asquarezone/LTTerraformModules.git//aws/securitygroup"
+  vpc_id = module.vpc.id
+  security_group_info = {
+    name        = "web"
+    description = "this opens 80 and 22 port"
+    inbound_rules = [{
+      port        = 22
+      protocol    = "tcp"
+      source      = "0.0.0.0/0"
+      description = "open ssh"
+      }, {
+      port        = 80
+      protocol    = "tcp"
+      source      = "0.0.0.0/0"
+      description = "open http"
+      }, {
+      port        = 443
+      protocol    = "tcp"
+      source      = "0.0.0.0/0"
+      description = "open https"
+    }]
+  }
+
 }
