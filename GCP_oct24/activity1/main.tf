c6a15c61f970d42a42213424585ed4b7c8270a66
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-10-25 19:52:56 +0530

    Added changes

---
+++ GCP_oct24/activity1/main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_network" "base" {
+  name                    = "activity1"
+  auto_create_subnetworks = false
+
+}
+
+resource "google_compute_subnetwork" "web" {
+  name          = "web"
+  network       = google_compute_network.base.id
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+
+}
+
+
+resource "google_compute_subnetwork" "db" {
+  name          = "db"
+  network       = google_compute_network.base.id
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+
+}
+
+
+
---
+++ GCP_oct24/activity1/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "peak-responder-438809-q5"
+
+}
