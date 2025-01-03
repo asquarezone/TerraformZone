29d38e78077077f92d362913330813b32f256839
shaikkhajaibrahim <shaikkhajaibrahim@live.in>
2025-01-03 08:38:39 +0530

    Added lint

--- Jan25/aws/activity1/main.tf
+++ Jan25/aws/activity1/main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.82.2"
+      version = ">= 5.82.2"
     }
   }
+  required_version = ">= 1.10.0"
 }
 
 provider "aws" {
