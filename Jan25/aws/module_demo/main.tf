0dc0469b7b3b77c0dc854d3f40b388fc162b4e55
shaikkhajaibrahim <shaikkhajaibrahim@live.in>
2025-01-18 10:02:33 +0530

    Added usage of community module

--- Jan25/aws/module_demo/main.tf
+++ Jan25/aws/module_demo/main.tf
@@ -14,6 +14,27 @@ module "myvpc" {
   }]
 
 }
+
+module "http_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  vpc_id = module.myvpc.vpc_id
+  name = "web"
+  ingress_with_cidr_blocks = [{
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    description = "open http"
+    cidr_blocks = "0.0.0.0/0"
+  }, {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    description = "open https"
+    cidr_blocks = "0.0.0.0/0"
+  }]
+}
+
 output "vpcid" {
   value = module.myvpc.vpc_id
 }
